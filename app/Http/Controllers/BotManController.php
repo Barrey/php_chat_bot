130ad922602c8526fbb12468accd371a6059cc05
Bari <ajibari@gmail.com>
2017-12-18 18:46:50 +0700

    bot laravel

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotMan;
+use Illuminate\Http\Request;
+use App\Conversations\ExampleConversation;
+
+class BotManController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function handle()
+    {
+        $botman = app('botman');
+
+        // our first BotMan command
+           $botman->hears('hello', 'App\Http\Controllers\Listen\HelloBotCommandsController@handleSayHello');
+
+           $botman->hears("hello I'm {name}", 'App\Http\Controllers\Listen\HelloBotCommandsController@handleSayHelloWithName');
+
+        $botman->listen();
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function tinker()
+    {
+        return view('tinker');
+    }
+
+    /**
+     * Loaded through routes/botman.php
+     * @param  BotMan $bot
+     */
+    public function startConversation(BotMan $bot)
+    {
+        $bot->startConversation(new ExampleConversation());
+    }
+}
